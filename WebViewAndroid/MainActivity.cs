f7c2fa51b0b305c6a119a378a4073635c4a1ebb9
Masataka Anime <Judas3991@gmail.com>
2020-07-18 22:31:27 -0500

    implement go back in web view

--- WebViewAndroid/MainActivity.cs
+++ WebViewAndroid/MainActivity.cs
@@ -1,14 +1,19 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Runtime;
 using Android.Support.V7.App;
+using Android.Views;
 using Android.Webkit;
 using Android.Widget;
+using Java.Lang;
 
 namespace WebViewAndroid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private WebView webView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -17,7 +22,7 @@ namespace WebViewAndroid
 
             EditText txtUrl = FindViewById<EditText>(Resource.Id.txtUrl);
 
-            WebView webView = FindViewById<WebView>(Resource.Id.webView);
+            webView = FindViewById<WebView>(Resource.Id.webView);
             webView.SetWebViewClient(new ExtendWebViewClient());
 
             WebSettings webSettings = webView.Settings;
@@ -31,6 +36,16 @@ namespace WebViewAndroid
 
                 webView.LoadUrl(txtUrl.Text);
             };
+        }
+
+        public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == Keycode.Back && webView.CanGoBack())
+            {
+                webView.GoBack();
+                return true;
+            }
+            return base.OnKeyDown(keyCode, e);
         }
     }
 
