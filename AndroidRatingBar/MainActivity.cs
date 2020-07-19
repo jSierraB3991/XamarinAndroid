086d12d2ff9f9269bb13b7bb847e0401c202993b
Masataka Anime <Judas3991@gmail.com>
2020-07-18 21:47:18 -0500

    rating bar ok

---
+++ AndroidRatingBar/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace AndroidRatingBar
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var ratingBar = FindViewById<RatingBar>(Resource.Id.ratingBar);
+            var txtRate = FindViewById<TextView>(Resource.Id.txtRating);
+
+            ratingBar.RatingBarChange += (s, e) => {
+                txtRate.Text = $"Rate {e.Rating}";
+            };
+        }
+    }
+}
