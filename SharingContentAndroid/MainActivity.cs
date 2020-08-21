f7942325e3f4a68a72eb8aa1d1b07fe453e0454e
Masataka Anime <Judas3991@gmail.com>
2020-08-21 00:40:35 -0500

    part of share complete

--- SharingContentAndroid/MainActivity.cs
+++ SharingContentAndroid/MainActivity.cs
@@ -3,6 +3,8 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Views;
+using Android.Content;
 
 namespace SharingContentAndroid
 {
@@ -16,11 +18,28 @@ namespace SharingContentAndroid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.share_menu, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_share:
+                    Intent sharing = new Intent(Intent.ActionSend);
+                    sharing.SetType("text/plain");
+                    sharing.PutExtra(Intent.ExtraSubject, "Subject Here");
+                    sharing.PutExtra(Intent.ExtraText, "This is content to share");
+                    sharing.PutExtra(Intent.ExtraTitle, "Title Here");
+                    StartActivity(Intent.CreateChooser(sharing, "Shearing Oprions"));
+                    return true;
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
         }
     }
 }
