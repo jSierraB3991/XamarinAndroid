95b7366879cfacff93825f0fb4c75e8479f4a9ce
Masataka Anime <Judas3991@gmail.com>
2020-10-26 23:55:20 -0500

    finish proyect, in spanish and english

---
+++ FingerPrintXam/FingerPrintHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace FingerPrintXam
+{
+    using Android;
+    using Android.Content;
+    using Android.Hardware.Fingerprints;
+    using Android.OS;
+    using Android.Support.V4.App;
+    using Android.Widget;
+
+    internal class FingerPrintHandler: FingerprintManager.AuthenticationCallback
+    {
+        private readonly Context _context;
+
+        public FingerPrintHandler(Context context)
+        {
+            this._context = context;
+        }
+
+        internal void StartAuthentication(FingerprintManager fingerprintManager, FingerprintManager.CryptoObject cryptoObject)
+        {
+            CancellationSignal cancellationSignal = new CancellationSignal();
+            if (ActivityCompat.CheckSelfPermission(_context, Manifest.Permission.UseFingerprint) != (int)Android.Content.PM.Permission.Granted)
+                return;
+            fingerprintManager.Authenticate(cryptoObject, cancellationSignal, 0, this, null);
+        }
+
+        public override void OnAuthenticationFailed()
+        {
+            Toast.MakeText(_context, _context.GetString(Resource.String.fingerprintFailed), ToastLength.Short).Show();
+
+        public override void OnAuthenticationSucceeded(FingerprintManager.AuthenticationResult result)
+        {
+            Toast.MakeText(_context, _context.GetString(Resource.String.fingerprintSuccess), ToastLength.Short).Show();
+            _context.StartActivity(new Intent(_context, typeof(HomeActivity)));
+        }
+    }
+}
--- FingerPrintXam/MainActivity.cs
+++ FingerPrintXam/MainActivity.cs
@@ -1,21 +1,96 @@
-﻿using Android.App;
-using Android.OS;
-using Android.Support.V7.App;
-using Android.Runtime;
-using Android.Widget;
-
-namespace FingerPrintXam
+﻿namespace FingerPrintXam
 {
+    using Android;
+    using Android.App;
+    using Android.Hardware.Fingerprints;
+    using Android.OS;
+    using Android.Runtime;
+    using Android.Security.Keystore;
+    using Android.Support.V4.App;
+    using Android.Support.V7.App;
+    using Android.Widget;
+    using Java.Security;
+    using Javax.Crypto;
+    using System;
+
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private KeyStore keyStore;
+        private Cipher cipher;
+        private string KEY_NAME = "JDSB";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            KeyguardManager keyguardManager = (KeyguardManager)GetSystemService(KeyguardService);
+            FingerprintManager fingerprintManager = (FingerprintManager)GetSystemService(FingerprintService);
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.UseFingerprint) != (int)Android.Content.PM.Permission.Granted)
+                return;
+            if (!fingerprintManager.IsHardwareDetected)
+            {
+                Toast.MakeText(this, GetString(Resource.String.fingerPrintPermissionDisabled), ToastLength.Short).Show();
+                return;
+            }
+            if (!fingerprintManager.HasEnrolledFingerprints)
+            {
+                Toast.MakeText(this, GetString(Resource.String.fingerPrintNotRegister), ToastLength.Short).Show();
+                return;
+            }
+            if (!keyguardManager.IsKeyguardSecure)
+            {
+                Toast.MakeText(this, GetString(Resource.String.lockscreenDisabled), ToastLength.Short).Show();
+                return;
+            }
+            GetKey();
+            if (CipherInit())
+            {
+                FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                FingerPrintHandler helper = new FingerPrintHandler(this);
+                helper.StartAuthentication(fingerprintManager, cryptoObject);
+            }
+
         }
+
+        private bool CipherInit()
+        {
+            try
+            {
+                cipher = Cipher.GetInstance(KeyProperties.KeyAlgorithmAes + "/" +
+                                            KeyProperties.BlockModeCbc + "/" +
+                                            KeyProperties.EncryptionPaddingPkcs7);
+                keyStore.Load(null);
+                IKey key = (IKey)keyStore.GetKey(KEY_NAME, null);
+                cipher.Init(CipherMode.EncryptMode, key);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+                return false;
+            }
+        }
+
+        private void GetKey()
+        {
+            string androidKeyStore = "AndroidKeyStore";
+
+            keyStore = KeyStore.GetInstance(androidKeyStore);
+            KeyGenerator keyGenerator = KeyGenerator.GetInstance(KeyProperties.KeyAlgorithmAes,
+                androidKeyStore);
+            keyStore.Load(null);
+            keyGenerator.Init(new KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyStorePurpose.Encrypt | KeyStorePurpose.Decrypt)
+                .SetBlockModes(KeyProperties.BlockModeCbc)
+                .SetUserAuthenticationRequired(true)
+                .SetEncryptionPaddings(KeyProperties.EncryptionPaddingPkcs7)
+                .Build());
+            keyGenerator.GenerateKey();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
