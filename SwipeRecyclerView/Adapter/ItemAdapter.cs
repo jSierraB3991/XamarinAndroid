49ba072eae5daaa475bf663190f962a27d3eb844
Masataka Anime <Judas3991@gmail.com>
2020-11-01 12:27:41 -0500

    parte para llenar la data finalizada

---
+++ SwipeRecyclerView/Adapter/ItemAdapter.cs
@@ -0,0 +1,40 @@
+﻿namespace SwipeRecyclerView.Adapter
+{
+    using Android.Content;
+    using Android.Support.V7.Widget;
+    using Android.Views;
+    using Com.Bumptech.Glide;
+    using SwipeRecyclerView.Holder;
+    using SwipeRecyclerView.Model;
+    using System.Collections.Generic;
+
+    public class ItemAdapter : RecyclerView.Adapter
+    {
+        private readonly Context _context;
+        private readonly List<Item> _items;
+
+        public ItemAdapter(Context context, List<Item> items)
+        {
+            this._context = context;
+            this._items = items;
+        }
+
+        public override int ItemCount => _items.Count;
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            var myViewHolder = holder as MyViewHolder;
+            Glide.With(this._context)
+                .Load(_items[position].Image)
+                .Into(myViewHolder.Image);
+            myViewHolder.TxtName.Text = _items[position].Name;
+            myViewHolder.TxtPrice.Text = _items[position].Price;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemVIew = LayoutInflater.From(this._context).Inflate(Resource.Layout.item_layout, parent, false);
+            return new MyViewHolder(itemVIew);
+        }
+    }
+}
---
+++ SwipeRecyclerView/Holder/MyViewHolder.cs
@@ -0,0 +1,23 @@
+﻿namespace SwipeRecyclerView.Holder
+{
+    using Android.Support.V7.Widget;
+    using Android.Views;
+    using Android.Widget;
+
+    public class MyViewHolder: RecyclerView.ViewHolder
+    {
+        public TextView TxtName { get; }
+
+        public TextView TxtPrice { get; }
+
+        public ImageView Image { get; }
+
+
+        public MyViewHolder(View viewItem): base(viewItem)
+        {
+            this.TxtName = ItemView.FindViewById<TextView>(Resource.Id.txtName);
+            this.TxtPrice = ItemView.FindViewById<TextView>(Resource.Id.txtPrice);
+            this.Image = ItemView.FindViewById<ImageView>(Resource.Id.cart_image);
+        }
+    }
+}
