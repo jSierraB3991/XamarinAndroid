918ef427a5a5f26bcdeccf61fb3df777f16e53c5
Masataka Anime <Judas3991@gmail.com>
2020-11-03 00:36:17 -0500

    project finish

---
+++ SqlLiteAndroid/Adapter/ListViewAdapter.cs
@@ -0,0 +1,57 @@
+﻿namespace SqlLiteAndroid.Adapter
+{
+    using Android.Content;
+    using Android.Runtime;
+    using Android.Support.V7.App;
+    using Android.Views;
+    using Android.Widget;
+    using SqlLiteAndroid.Model;
+    using System.Collections.Generic;
+
+    public class ListViewAdapter : BaseAdapter
+    {
+
+        private readonly AppCompatActivity _context;
+        private readonly List<Item> _items;
+
+        public ListViewAdapter(AppCompatActivity context, List<Item> items)
+        {
+            this._context = context;
+            this._items = items;
+        }
+
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position) => _items[position].Id;
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = _context.LayoutInflater.Inflate(Resource.Layout.item_data, parent, false);
+            var name = view.FindViewById<TextView>(Resource.Id.tvName);
+            var age = view.FindViewById<TextView>(Resource.Id.tvAge);
+            var email = view.FindViewById<TextView>(Resource.Id.tvEmail);
+
+            name.Text = _items[position].Name;
+            age.Text = _items[position].Age;
+            email.Text = _items[position].Email;
+
+            return view;
+        }
+
+        public override int Count => _items.Count;
+
+    }
+
+    public class ListViewAdapterViewHolder : Java.Lang.Object
+    {
+        public TextView Name { get; set; }
+
+        public TextView Age { get; set; }
+
+        public TextView Email { get; set; }
+    }
+}
---
+++ SqlLiteAndroid/Data/DatabaseLite.cs
@@ -0,0 +1,133 @@
+﻿namespace SqlLiteAndroid.Data
+{
+    using Android.Util;
+    using SQLite;
+    using SqlLiteAndroid.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class DatabaseLite
+    {
+        string _folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+        public bool CreateDatabase()
+        {
+            try
+            {
+                using (var con = new SQLiteConnection(Path.Combine(_folder, "xamSqliteItem.db")))
+                {
+                    con.CreateTable<Item>();
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SqlException", ex.Message);
+                return false;
+            }
+        }
+
+        public Item InserItem(Item item)
+        {
+            try
+            {
+                using (var con = new SQLiteConnection(Path.Combine(_folder, "xamSqliteItem.db")))
+                {
+                    con.Insert(item);
+                    return item;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SqlException", ex.Message);
+                return null;
+            }
+        }
+
+        public List<Item> SelectTableItems()
+        {
+            try
+            {
+                using (var con = new SQLiteConnection(Path.Combine(_folder, "xamSqliteItem.db")))
+                {
+                    return con.Table<Item>().ToList();
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SqlException", ex.Message);
+                return null;
+            }
+        }
+
+        public Item SelectTableItemById(int id)
+        {
+            try
+            {
+                using (var con = new SQLiteConnection(Path.Combine(_folder, "xamSqliteItem.db")))
+                {
+                    return con.Table<Item>().FirstOrDefault(i => i.Id.Equals(id));
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SqlException", ex.Message);
+                return null;
+            }
+        }
+
+        public bool UpdateItem(Item item)
+        {
+            try
+            {
+                using (var con = new SQLiteConnection(Path.Combine(_folder, "xamSqliteItem.db")))
+                {
+                    con.Query<Item>("UPDATE Item Set Name = ?, Age = ?, Email = ? WHERE id = ?",
+                        item.Name, item.Age, item.Email, item.Id);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SqlException", ex.Message);
+                return false;
+            }
+        }
+
+        public bool DeleteIem(Item item)
+        {
+            try
+            {
+                using (var con = new SQLiteConnection(Path.Combine(_folder, "xamSqliteItem.db")))
+                {
+                    con.Delete(item);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SqlException", ex.Message);
+                return false;
+            }
+        }
+
+        public bool ExistsItem(int id) => this.SelectTableItemById(id) != null;
+
+        public void DeleteItems()
+        {
+            try
+            {
+                using (var con = new SQLiteConnection(Path.Combine(_folder, "xamSqliteItem.db")))
+                {
+                    con.DropTable<Item>();
+                    con.CreateTable<Item>();
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SqlException", ex.Message);
+            }
+        }
+    }
+}
