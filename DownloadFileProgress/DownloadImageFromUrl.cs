5f12ddda46e638e085c4520682fa2c30c8de9079
Masataka Anime <Judas3991@gmail.com>
2020-11-04 21:50:24 -0500

    finish project

---
+++ DownloadFileProgress/DownloadImageFromUrl.cs
@@ -0,0 +1,87 @@
+﻿namespace DownloadFileProgress
+{
+    using Android.App;
+    using Android.Content;
+    using Android.Graphics.Drawables;
+    using Android.OS;
+    using Android.Widget;
+    using Java.IO;
+    using Java.Net;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    public class DownloadImageFromUrl : AsyncTask<string, string, string>
+    {
+        private readonly Context context;
+        private readonly ImageView imageView;
+        private ProgressDialog progressDialog;
+        private string fileName;
+
+        public DownloadImageFromUrl(Context context, ImageView imageView)
+        {
+            this.context = context;
+            this.imageView = imageView;
+            this.fileName = Guid.NewGuid().ToString();
+        }
+
+        protected override void OnPreExecute()
+        {
+            progressDialog = new ProgressDialog(context);
+            progressDialog.SetMessage("Download File. Please Wait...");
+            progressDialog.Indeterminate = false;
+            progressDialog.Max = 100;
+            progressDialog.SetProgressStyle(ProgressDialogStyle.Spinner);
+            progressDialog.SetCancelable(true);
+            progressDialog.Show();
+            base.OnPreExecute();
+        }
+
+        protected override void OnProgressUpdate(params string[] values)
+        {
+            base.OnProgressUpdate(values);
+            progressDialog.SetProgressNumberFormat(values[0]);
+            progressDialog.Progress = int.Parse(values[0]);
+        }
+
+        protected override void OnPostExecute([AllowNull] string result)
+        {
+            var storagePath = Android.OS.Environment.ExternalStorageDirectory.Path;
+            var filePath = System.IO.Path.Combine(storagePath, $"{fileName}.jpg");
+            progressDialog.Dismiss();
+            imageView.SetImageDrawable(Drawable.CreateFromPath(filePath));
+        }
+
+        protected override string RunInBackground(params string[] @params)
+        {
+            var storagePath = Android.OS.Environment.ExternalStorageDirectory.Path;
+            var filePath = System.IO.Path.Combine(storagePath, $"{fileName}.jpg");
+            int count = 0;
+            try
+            {
+                URL url = new URL(@params[0]);
+                URLConnection connection = url.OpenConnection();
+                connection.Connect();
+                int lengthOfFile = connection.ContentLength;
+                InputStream input = new BufferedInputStream(url.OpenStream(), lengthOfFile);
+                OutputStream output = new FileOutputStream(filePath);
+
+                byte[] data = new byte[1024];
+                long total = 0;
+                while ((count = input.Read(data)) != -1)
+                {
+                    total += count;
+                    PublishProgress($"{(int)(total / 100) / lengthOfFile}");
+                    output.Write(data, 0, count);
+                }
+                output.Flush();
+                output.Close();
+                input.Close();
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
--- DownloadFileProgress/MainActivity.cs
+++ DownloadFileProgress/MainActivity.cs
@@ -4,6 +4,7 @@
     using Android.OS;
     using Android.Support.V7.App;
     using Android.Runtime;
+    using Android.Widget;
 
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
@@ -14,11 +15,18 @@
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var btnMain = FindViewById<Button>(Resource.Id.btn_main);
+            var imageView = FindViewById<ImageView>(Resource.Id.imageview_main);
+            btnMain.Click += delegate {
+                var download = new DownloadImageFromUrl(this, imageView);
+                download.Execute("https://images.dog.ceo/breeds/bullterrier-staffordshire/n02093256_8623.jpg");
+            };
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
