caf0549e2f33d2813d3d0bac98fb0e59dcb0c28e
Masataka Anime <Judas3991@gmail.com>
2020-11-05 22:09:04 -0500

    finish project

--- TakePictureCamera/MainActivity.cs
+++ TakePictureCamera/MainActivity.cs
@@ -4,16 +4,40 @@
     using Android.OS;
     using Android.Support.V7.App;
     using Android.Runtime;
+    using Android.Widget;
+    using Android.Content;
+    using Android.Provider;
+    using Android.Graphics;
 
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private ImageView imageView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            var button = FindViewById<Button>(Resource.Id.btn_main);
+            imageView = FindViewById<ImageView>(Resource.Id.image_view_main);
+            button.Click += Button_Click;
         }
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            var bitmap = (Bitmap)data.Extras.Get("data");
+            imageView.SetImageBitmap(bitmap);
+        }
+
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
