3afd8a8383087d13a3d6cb856973a8cb93a6914c
Masataka Anime <Judas3991@gmail.com>
2020-11-06 20:52:56 -0500

    finish project

--- Clipboard/MainActivity.cs
+++ Clipboard/MainActivity.cs
@@ -22,14 +22,20 @@
             var btnCopy = FindViewById<Button>(Resource.Id.btn_copy);
             var btnPaste = FindViewById<Button>(Resource.Id.btn_paste);
 
-            var txtCopy = FindViewById<TextView>(Resource.Id.txt_copy);
-            var txtPaste = FindViewById<TextView>(Resource.Id.txt_paste);
+            var txtCopy = FindViewById<EditText>(Resource.Id.txt_copy);
+            var txtPaste = FindViewById<TextView> (Resource.Id.txt_paste);
 
             clipboardManager = (ClipboardManager)GetSystemService(ClipboardService);
             btnCopy.Click += delegate {
                 var copyText = txtCopy.Text;
                 clipData = ClipData.NewPlainText("text", copyText);
                 clipboardManager.PrimaryClip = clipData;
+                Toast.MakeText(this, Resource.String.msg_copy_text, ToastLength.Short).Show();
+            };
+            btnPaste.Click += delegate {
+                ClipData.Item item = clipboardManager.PrimaryClip.GetItemAt(0);
+                txtPaste.Text = item.Text;
+                Toast.MakeText(this, Resource.String.msg_paste_text, ToastLength.Short).Show();
             };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
