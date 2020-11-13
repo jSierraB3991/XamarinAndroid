ee286b80ea2397cef2d3d8a5b34913830a88a599
Masataka Anime <Judas3991@gmail.com>
2020-11-13 17:30:59 -0500

    Get Data for api

---
+++ LoadMore/Model/Publish.cs
@@ -0,0 +1,15 @@
+﻿namespace LoadMore.Model
+{
+    internal class Publish
+    {
+        public int Id { get; set; }
+
+        public int IdUser { get; set; }
+
+        public System.DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ LoadMore/Service/ISocialMediaService.cs
@@ -0,0 +1,12 @@
+﻿namespace LoadMore.Service
+{
+    using LoadMore.Model;
+    using Refit;
+    using System.Threading.Tasks;
+
+    internal interface ISocialMediaService
+    {
+        [Get("/Publish?PageNumber={pageNumber}")]
+        Task<RequestPublish> GetPublish(int pageNumber);
+    }
+}
