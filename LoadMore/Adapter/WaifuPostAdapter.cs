0215db6535eff988d371acc0b62fcb4650f66921
Masataka Anime <Judas3991@gmail.com>
2020-11-13 18:17:11 -0500

    first part Ok

---
+++ LoadMore/Adapter/WaifuPostAdapter.cs
@@ -0,0 +1,51 @@
+﻿using Android.Content;
+namespace LoadMore.Adapter
+{
+    using Android.Support.V7.Widget;
+    using Android.Views;
+    using Android.Widget;
+    using LoadMore.Model;
+    using Square.Picasso;
+    using System;
+    using System.Collections.Generic;
+
+    internal class WaifuPostAdapter : RecyclerView.Adapter
+    {
+        private readonly Context _ctx;
+        private readonly List<Publish> _data;
+
+        public WaifuPostAdapter(Context ctx, List<Publish> data)
+        {
+            this._ctx = ctx;
+            this._data = data;
+        }
+
+        public override int ItemCount => _data.Count;
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            var waifuHolader = holder as WaifuHolder;
+            Picasso.Get().Load(_data[position].Image).Into(waifuHolader.ImageWaifu);
+            waifuHolader.NameText.Text = _data[position].Description.Substring(0, 20);
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            var itemView = LayoutInflater.From(_ctx).Inflate(Resource.Layout.waifu_item_layout, parent, false);
+            return new WaifuHolder(itemView);
+        }
+    }
+
+    internal class WaifuHolder : RecyclerView.ViewHolder
+    {
+        public ImageView ImageWaifu { get; set; }
+        
+        public TextView NameText { get; set; }
+
+        public WaifuHolder(View itemView) : base(itemView)
+        {
+            ImageWaifu = itemView.FindViewById<ImageView>(Resource.Id.image_waifu_item);
+            NameText = itemView.FindViewById<TextView>(Resource.Id.text_waifu_name);
+        }
+    }
+}
--- LoadMore/MainActivity.cs
+++ LoadMore/MainActivity.cs
@@ -2,24 +2,72 @@
 {
     using Android.App;
     using Android.OS;
-    using Android.Support.V7.App;
     using Android.Runtime;
+    using Android.Support.V4.Widget;
+    using Android.Support.V7.App;
+    using Android.Support.V7.Widget;
+    using Android.Widget;
+    using EDMTDialog;
+    using LoadMore.Adapter;
+    using LoadMore.Service;
+    using System.Threading.Tasks;
+    using static Android.Support.V4.Widget.SwipeRefreshLayout;
 
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, IOnRefreshListener
     {
+        private RecyclerView _recyclerView;
+        private SwipeRefreshLayout _swipeRefresh;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            _swipeRefresh = FindViewById<SwipeRefreshLayout>(Resource.Id.swipe_main);
+            _swipeRefresh.SetColorScheme(Resource.Color.colorPrimary,
+                Android.Resource.Color.HoloGreenDark,
+                Android.Resource.Color.HoloBlueDark,
+                Android.Resource.Color.HoloOrangeDark);
+            _swipeRefresh.SetOnRefreshListener(this);
+            _swipeRefresh.Post(() => { GetData(); });
+            _recyclerView = FindViewById<RecyclerView>(Resource.Id.recycler_waifu);
+            _recyclerView.HasFixedSize = true;
+            _recyclerView.SetLayoutManager(new GridLayoutManager(this, 2));
         }
+
+        public async Task GetData()
+        {
+            var service = new SocialMediaService();
+            Android.Support.V7.App.AlertDialog dialog = new EDMTDialogBuilder()
+                .SetContext(this).SetMessage(GetString(Resource.String.msg_please_wait)).Build();
+            if (!_swipeRefresh.Refreshing)
+                dialog.Show();
+
+            var result = await service.GetData(this, 1);
+            if (!result.IsSuccess)
+            {
+                if (!_swipeRefresh.Refreshing)
+                    dialog.Dismiss();
+                _swipeRefresh.Refreshing = false;
+                Toast.MakeText(this, result.Message, ToastLength.Short).Show();
+                return;
+            }
+            _recyclerView.SetAdapter(new WaifuPostAdapter(this, result.Result.Data));
+            if (!_swipeRefresh.Refreshing)
+                dialog.Dismiss();
+            _swipeRefresh.Refreshing = false;
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public async void OnRefresh()
+        {
+            await GetData();
         }
     }
 }
