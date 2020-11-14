8edc89acafade7d9f6522d703dde14cbe4798d65
Masataka Anime <Judas3991@gmail.com>
2020-11-13 20:57:52 -0500

    finish infinite Cycler View

---
+++ InfiniteCycleView/Adapter/AnimePagerAdapter.cs
@@ -0,0 +1,56 @@
+﻿namespace InfiniteCycleView.Adapter
+{
+    using Android.Content;
+    using Android.Support.V4.View;
+    using Android.Views;
+    using Android.Widget;
+    using Square.Picasso;
+    using System.Collections.Generic;
+
+    internal class AnimePagerAdapter : PagerAdapter
+    {
+        private readonly Context _ctx;
+        private readonly List<Model.Model> _favorites;
+
+        public AnimePagerAdapter(Context ctx, List<Model.Model> favorites)
+        {
+            this._ctx = ctx;
+            this._favorites = favorites;
+        }
+
+        public override int Count => _favorites.Count;
+
+        public override bool IsViewFromObject(View view, Java.Lang.Object @object) => view == @object;
+
+        public override void DestroyItem(ViewGroup container, int position, Java.Lang.Object @object)
+        {
+            container.RemoveView((View)@object);
+        }
+
+        public override Java.Lang.Object InstantiateItem(ViewGroup container, int position)
+        {
+            var view = LayoutInflater.From(_ctx)
+                .Inflate(Resource.Layout.anime_item_layout, container, false);
+
+            var image = view.FindViewById<ImageView>(Resource.Id.img_photo_title);
+            var title = view.FindViewById<TextView>(Resource.Id.text_name_item);
+            var description = view.FindViewById<TextView>(Resource.Id.text_item_description);
+            var btnFloatin = view.FindViewById<Android.Support.Design.Widget.FloatingActionButton>(Resource.Id.btn_fav_item);
+
+            Picasso.Get().Load(_favorites[position].ImageUrl).Into(image);
+            title.Text = _favorites[position].Name;
+            description.Text = _favorites[position].Description;
+            view.Click += delegate
+            {
+                Toast.MakeText(_ctx, _favorites[position].Name, ToastLength.Short).Show();
+            };
+            btnFloatin.Click += delegate
+            {
+                var msg = string.Format(_ctx.GetString(Resource.String.msg_click_fav), _favorites[position].Name);
+                Toast.MakeText(_ctx, msg, ToastLength.Short).Show();
+            };
+            container.AddView(view);
+            return view;
+        }
+    }
+}
--- InfiniteCycleView/MainActivity.cs
+++ InfiniteCycleView/MainActivity.cs
@@ -6,18 +6,22 @@ using Android.Widget;
 using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Com.Gigamole.Infinitecycleviewpager;
+using InfiniteCycleView.Adapter;
 
 namespace InfiniteCycleView
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private HorizontalInfiniteCycleViewPager infiniteCycleViewPager;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            infiniteCycleViewPager = FindViewById<HorizontalInfiniteCycleViewPager>(Resource.Id.pager_main);
             GetData();
         }
 
@@ -31,17 +35,16 @@ namespace InfiniteCycleView
             }
             Toast.MakeText(this, response.Result[0].Name, ToastLength.Short).Show();
             LoadData(response.Result);
-
         }
 
         public void LoadData(List<Model.Model> data)
-        { 
+        {
+            infiniteCycleViewPager.Adapter = new AnimePagerAdapter(this, data);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
